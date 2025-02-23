9b4bbe19c0546328002d2009a26654dbe8bc001c
selva-ganesh-m <selvadev2k@gmail.com>
2025-02-23 18:42:23 +0530

    init: Simple/Complex Calc

---
+++ src/complex/ComplexCalc.ts
@@ -0,0 +1,5 @@
+export class ComplexCalc {
+    power(base: number, exponent: number): number {
+        return Math.pow(base, exponent);
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { SimpleCalc } from "./simple/SimpleCalc";
+import { ComplexCalc } from "./complex/ComplexCalc";
+
+export { SimpleCalc, ComplexCalc };
